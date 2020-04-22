edb252bd785c60f162daa2a0721d57901b201846
Audrey <audrey.naidu.21@bishops.com>
2020-04-22 10:43:34 -0700

    I think this works

--- detectvideo.py
+++ detectvideo.py
@@ -38,32 +38,35 @@ while True:
     orig = frame.copy()
 
     # detect people in the image
-#    (rects, weights) = hog.detectMultiScale(orig, winStride=(4, 4),
-#        padding=(8, 8), scale=1.05)
+    (rects, weights) = hog.detectMultiScale(orig, winStride=(4, 4),
+        padding=(8, 8), scale=1.05)
 
     # draw the original bounding boxes
- #   for (x, y, w, h) in rects:
- #       cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    for (x, y, w, h) in rects:
+        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     # apply non-maxima suppression to the bounding boxes using a
     # fairly large overlap threshold to try to maintain overlapping
     # boxes that are still people
-#    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-#    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
     # draw the final bounding boxes
- #   for (xA, yA, xB, yB) in pick:
- #       cv2.rectangle(orig, (xA, yA), (xB, yB), (0, 255, 0), 2)
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(orig, (xA, yA), (xB, yB), (0, 255, 0), 2)
         
     
     # show some information on the number of bounding boxes
     #filename = imagePath[imagePath.rfind("/") + 1:]
-    #print("[INFO] {}: {} original boxes, {} after suppression".format(
-       # filename, len(rects), len(pick)))
+    filename = "one"
+    print("[INFO] {}: {} original boxes, {} after suppression".format(
+        filename, len(rects), len(pick)))
 
     # show the output images
-#    cv2.imshow("Before NMS", orig)
+    #cv2.imshow("Before NMS", frame)
     cv2.imshow("After NMS", orig)
+    
+    cv2.waitKey(1)
  
 
     
